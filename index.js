e2ce12a7eb41ae8b422fe3028f9f877384e2450f
Alejandro Marcano <+>
2021-09-22 00:21:44 -0400

    completed assignemtn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate!') {
+    console.log(`This Saturday, I want to ${activity}`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+// function mondayWork(work = 'go to the office'){
+   // console.log(`This Monday, I will ${work}`);
+// }
+//mondayWork();
+//mondayWork("work from home");
+
+const fn = function mondayWork (work = "go to the office") {
+    console.log(`This Monday, ${work}`); }
+fn();
+fn("work from home");
+
+function wrapAdjective(special) {
+    const word1 = "you are"
+    return function () {
+        const word2 = "where";
+        return function (){
+            console.log(`${word1} ${word2} ${special}`);
+        };
+
+    };
+}
+wrapAdjective(" you need to be dedicated programmer")()();
